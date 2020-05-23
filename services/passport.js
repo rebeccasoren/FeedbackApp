3a7206e16efbcd0b755f2302ac216629174183b9
Rebecca Soren <sorenrebeccashruti@gmail.com>
2020-05-23 08:06:10 +0530

    making changes

--- services/passport.js
+++ services/passport.js
@@ -19,17 +19,14 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleID:profile.id})
-            .then((existingUser)=>{
-                if(existingUser){
-                    done(null, existingUser);
-                } else {
-                    new User({googleID: profile.id})
-                    .save()
-                    .then((user) => done(null,user));
-                }
-            });
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+            const existingUser =await User.findOne({googleID:profile.id})
+            if(existingUser){
+                return done(null, existingUser);
+            }
+            const user = await new User({googleID: profile.id})
+            done(null,user);   
         }
     )
 );
